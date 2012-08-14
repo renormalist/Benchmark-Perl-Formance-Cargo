51d79f65c29f0aaeffa601517d4b074bc2cdc28d
Steffen Schwigon <ss5@renormalist.net>
2012-08-14 22:08:18 +0200

    fix sync script

--- sync_P6STD.sh
+++ sync_P6STD.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
 
-FROM="../perl6-std/"
+FROM="perl6-std/"
 TO="lib/auto/Benchmark/Perl/Formance/Cargo/P6STD/"
+
+git clone git@github.com:perl6/std.git $FROM
+
+pushd .
+cd $FROM
+git clean -dxf
+popd
 
 mkdir -p $TO
 rsync -a $FROM $TO
@@ -9,5 +16,7 @@ rsync -a $FROM $TO
 pushd .
 cd $TO
 rm -fr .git .gitignore
-perl -pni -e 's/^package /package # hide from indexer\n /' $(find -name "*.pm")
+perl -pni -e 's/^package (?!#)/package # hide from indexer\n /' $(find -name "*.pm")
 popd
+
+rm -fr $FROM
